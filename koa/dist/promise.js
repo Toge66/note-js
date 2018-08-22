abcaae47fb9bb693c8b920028e960e71d0148ff7
toge <toge0223@163.com>
2018-08-22 10:28:54 +0800

    删除文件

--- koa/dist/promise.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-var util = require('util');
-
-util.promisify(fs.readFile)('../package.json').then(JSON.parse).then(function (data) {
-  console.log(data);
-});
-//# sourceMappingURL=promise.js.map
