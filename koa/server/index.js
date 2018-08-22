4e54c480a834a1c3d1412e744cfaf85a0e576bc7
toge <toge0223@163.com>
2018-08-22 10:33:00 +0800

    更新package

--- koa/server/index.js
+++ koa/server/index.js
@@ -2,6 +2,8 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(async (ctx, next) => {
+  console.log(ctx.href)
+  console.log(ctx.request.href)
   ctx.body = 'hhahahahha'
   await next()
 })
