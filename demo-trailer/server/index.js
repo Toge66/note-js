b9796599a69516bea5ddb44b37614290360f7233
toge <toge0223@163.com>
2018-09-10 11:28:28 +0800

    Pugjs 尝试

--- demo-trailer/server/index.js
+++ demo-trailer/server/index.js
@@ -1,10 +1,20 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
 const app = new Koa()
+const views = require('koa-views')
+const { resolve } = require('path')
 
 app.use(logger())
+app.use(views(resolve(__dirname,'./views'),{
+  extension: 'pug'
+}))
 app.use(async (ctx, next) => {
-  ctx.body = '首页起来了'
+  await ctx.render('index', {
+    data:{
+      first: "Hello this is Koa-views render PUG template",
+      second: "koa-view test"
+    } 
+  })
 })
 
 app.listen(4455)
