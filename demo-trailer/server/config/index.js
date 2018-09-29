62e0a42bbdbfac572ff51878b735a01046087f95
toge <toge0223@163.com>
2018-09-29 16:26:03 +0800

    删除

--- demo-trailer/server/config/index.js
+++ demo-trailer/server/config/index.js
@@ -1,8 +1,3 @@
 module.exports = {
-    qiniu: {
-        "bucket": "",
-        "video": "",
-        "AK": "YVMKjKC6Q9be15KfBOhLV6DOnD53x8329-psh9dt",
-        "SK": "uIUXSRJ9HsL0RbuCcyZVMl8cUagcKSW6gC7KzuJ-"
-    }
+    
 }
