c23e19e10b9cd2c0812629eb3d9117ba564ae60d
toge <toge0223@163.com>
2018-10-10 17:17:53 +0800

    create collections

---
+++ demo-trailer/server/database/schema/movie.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const Mixed = Schema.Types.Mixed
+const ObjectId = Schema.Types.ObjectId
+
+const movieSchema = new Schema({
+    doubanId: {
+        unique: true,
+        type: String
+    },
+    categorys: {
+        type: ObjectId,
+        ref: 'Category'
+    },
+    rate: Number,
+    title: String,
+    summary: String,
+    video: String,
+    poster: String,
+    cover: String,
+
+    videoKey: String,
+    posterKey: String,
+    coverKey: String,
+
+    rawTitle: String,
+    movieTypes: [String],
+    pubdate: Mixed,
+    year: Number,
+    tag: [Array],
+    meta: {
+        createAt: {
+            type: Date,
+            default:Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default:Date.now()
+        }
+    }
+})
+
+movieSchema.pre('save', next => {
+    if(this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now()
+    }else {
+        this.meta.updateAt = Date.now()
+    }
+})
+
+mongoose.model('Movie', movieSchema)
---
+++ demo-trailer/server/database/schema/user.js
@@ -0,0 +1,112 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Schema = mongoose.Schema
+
+const SALT_WORK_FACTOR = 10
+const MAX_LOGIN_ATTEMPTS = 5
+const LOCK_TIME = 2 * 60 * 60 * 1000
+
+const userSchema = new Schema({
+    username: {
+        unique: true,
+        required: true,
+        type: String
+    },
+    password: {
+        unique: true,
+        required: true,
+        type: String
+    },
+    email: {
+        unique: true,
+        required: true,
+        type: String
+    },
+    loginAttempts: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    lockUntil: Number,
+    meta: {
+        createAt: {
+            type: Date,
+            default:Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default:Date.now()
+        }
+    }
+})
+
+userSchema.pre('save', next => {
+    if(this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now()
+    }else {
+        this.meta.updateAt = Date.now()
+    }
+    next()
+})
+
+userSchema.virtual('isLocked').get(() => {
+    return !!(this.lockUntil && this.lockUntil > Date.now())
+})
+
+userSchema.pre('save', next => {
+    if(!this.isModified('password')) return next()
+    bcrypt.getSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) return next()
+        bcrypt.hash(this.password, salt, (error, hashPwd) => {
+            if(error) return next()
+            this.password = hashPwd
+            next()
+        })
+    })
+})
+
+userSchema.methods = {
+    comparePwd: (_pwd, pwd) => {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(_pwd, pwd, (err, isMatch) => {
+                if (!!err) return reject(err)
+                resolve(isMatch)
+            })
+        })
+    },
+    inLogininAttepts: (user) => {
+        return new Promise((resolve, reject) => {
+            if(this.lockUntil && this.lockUntil < Date.now()) {
+                const update = {
+                    $set: {
+                        loginAttempts: 1
+                    },
+                    $unset: {
+                        lockUntil: 1
+                    }
+                }
+                this.update(update, err => {
+                    if(err) return reject(err)
+                    resolve(true)
+                })
+            }else {
+                const update = {
+                    $inc: {
+                        loginAttempts: 1
+                    }
+                }
+                if(this.loginAttempts + 1 >= MAX_LOGIN_ATTEMPTS && !this.isLocked) {
+                    update.$set = {
+                        lockUntil: Date.now() + LOCK_TIME
+                    }
+                }
+                this.update(update, err => {
+                    if(err) return reject(err)
+                    resolve(true)
+                })
+            }
+        })
+    }
+}
+
+mongoose.model('User', userSchema)
