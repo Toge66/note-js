fcb01c4870f22bb9f1022dd8b4174556f0f40bcf
toge <toge0223@163.com>
2018-12-13 10:59:00 +0800

    上传七牛

--- demo-trailer/server/index.js
+++ demo-trailer/server/index.js
@@ -1,30 +1,33 @@
-const Koa = require('koa')
-const logger = require('koa-logger')
-const app = new Koa()
-const views = require('koa-views')
-const { resolve } = require('path')
-const { connect, initSchema } = require('./database/init')
-const mongoose = require('mongoose')
+const Koa = require("koa");
+const logger = require("koa-logger");
+const app = new Koa();
+const views = require("koa-views");
+const { resolve } = require("path");
+const { connect, initSchema } = require("./database/init");
+const mongoose = require("mongoose");
 
-;(async () => {
-  await connect()
-  initSchema()
+(async () => {
+  await connect();
+  initSchema();
   // require('./tasks/movie-list')
   // require('./tasks/trailer')
-  require('./tasks/api')
-})()
-app.use(logger())
-app.use(views(resolve(__dirname,'./views'),{
-  extension: 'pug'
-}))
+  // require('./tasks/api')
+  require("./tasks/qiniu");
+})();
+app.use(logger());
+app.use(
+  views(resolve(__dirname, "./views"), {
+    extension: "pug"
+  })
+);
 app.use(async (ctx, next) => {
-  await ctx.render('index', {
-    data:{
+  await ctx.render("index", {
+    data: {
       first: "Hello this is Koa-views render PUG template",
       second: "koa-view test"
-    } 
-  })
-  next()
-})
+    }
+  });
+  next();
+});
 
-app.listen(4455)
+app.listen(4455);
--- demo-trailer/server/tasks/qiniu.js
+++ demo-trailer/server/tasks/qiniu.js
@@ -1,58 +1,62 @@
-const qiniu = require('qiniu')
-const nanoid = require('nanoid')
-const config = require('../config')
-const { qiniu: { bucket, AK, SK } } = config
+const qiniu = require("qiniu");
+const nanoid = require("nanoid");
+const config = require("../config");
+const mongoose = require("mongoose");
+const {
+  qiniu: { bucket, AK, SK }
+} = config;
+const Movie = mongoose.model("Movie");
 
-const mac = new qiniu.auth.digest.Mac(AK,SK)
+const mac = new qiniu.auth.digest.Mac(AK, SK);
 
-var cfg = new qiniu.conf.Config()
+var cfg = new qiniu.conf.Config();
 //config.useHttpsDomain = true;
-config.zone = qiniu.zone.Zone_z0
-var bucketManager = new qiniu.rs.BucketManager(mac, cfg)
+config.zone = qiniu.zone.Zone_z0;
+var bucketManager = new qiniu.rs.BucketManager(mac, cfg);
 
 const uploadToQiniu = (url, key) => {
-    return new Promise((resolve, reject) => {
-        bucketManager.fetch(url, bucket, key, (err, respBody, respInfo) => {
-            if (err) {
-                reject(err)
-              } else {
-                if (respInfo.statusCode == 200) {
-                  resolve(respBody.key)
-                } else {
-                  reject(respInfo)
-                }
-              }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    bucketManager.fetch(url, bucket, key, (err, respBody, respInfo) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (respInfo.statusCode == 200) {
+          resolve(respBody.key);
+        } else {
+          reject(respInfo);
+        }
+      }
+    });
+  });
+};
 
 (async () => {
-    const movies = [{
-        id: '26979545',
-        cover: 'https://img3.doubanio.com/img/trailer/medium/2502618393.jpg?',
-        video: 'http://vt1.doubanio.com/201809272114/2ef4691b974ab857d3d22658d4720e63/view/movie/M/302220893.mp4',
-        title: '蓝色星球2',
-        rate: 9.9,
-        poster: 'https://img1.doubanio.com/view/photo/l_ratio_poster/public/p2518413249.jpg'
-    }]
-    movies.forEach(async (item) => {
-        try{
-            const videoQiniuKey = await uploadToQiniu(item.video, nanoid())
-            const coverQiniuKey = await uploadToQiniu(item.cover, nanoid())
-            const posterQiniuKey = await uploadToQiniu(item.poster, nanoid())
+  // const movies = [{
+  //     id: '26979545',
+  //     cover: 'https://img3.doubanio.com/img/trailer/medium/2502618393.jpg?',
+  //     video: 'http://vt1.doubanio.com/201809272114/2ef4691b974ab857d3d22658d4720e63/view/movie/M/302220893.mp4',
+  //     title: '蓝色星球2',
+  //     rate: 9.9,
+  //     poster: 'https://img1.doubanio.com/view/photo/l_ratio_poster/public/p2518413249.jpg'
+  // }]
+  const movies = await Movie.find();
+  movies.forEach(async item => {
+    try {
+      const videoQiniuKey = await uploadToQiniu(item.video, nanoid());
+      const coverQiniuKey = await uploadToQiniu(item.cover, nanoid());
+      const posterQiniuKey = await uploadToQiniu(item.poster, nanoid());
 
-            if(videoQiniuKey) {
-                item.videoQiniuKey = videoQiniuKey
-            }
-            if(coverQiniuKey) {
-                item.coverQiniuKey = coverQiniuKey
-            }
-            if(posterQiniuKey) {
-                item.posterQiniuKey = posterQiniuKey
-            }
-        }
-        catch (err) {
-            console.log(err);
-        }
-    })
-})()
+      if (videoQiniuKey) {
+        item.videoQiniuKey = videoQiniuKey;
+      }
+      if (coverQiniuKey) {
+        item.coverQiniuKey = coverQiniuKey;
+      }
+      if (posterQiniuKey) {
+        item.posterQiniuKey = posterQiniuKey;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+})();
